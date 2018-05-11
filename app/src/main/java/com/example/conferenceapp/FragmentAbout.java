9c269a0f994358bd8f7a7d7db3a5aa96b31b00d1
Saatvik Jain <saatvik16261@iiitd.ac.in>
2018-05-11 23:06:50 +0530

    About page and icons

--- app/src/main/java/com/example/conferenceapp/FragmentAbout.java
+++ app/src/main/java/com/example/conferenceapp/FragmentAbout.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import mehdi.sakout.aboutpage.AboutPage;
+
+import static android.support.v4.os.LocaleListCompat.create;
+
 /**
  * Created by meghna on 8/1/18.
  */
@@ -18,13 +22,20 @@ public class FragmentAbout extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        View aboutPage = new AboutPage(getContext()).isRTL(false)
+                .setDescription(getString(R.string.description))
+                .setImage(R.drawable.logo)
+                .addEmail("chi@gmail.com")
+                .addFacebook("acmchi")
+                .addTwitter("sig_chi")
+                .create();
+        return aboutPage;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
 
 
     }
