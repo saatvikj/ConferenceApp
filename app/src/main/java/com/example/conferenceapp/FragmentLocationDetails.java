66cf37b6820ded40f43e2997ce919d5f6a2369c0
Meghna Gupta <meghnagupta64@gmail.com>
2018-05-27 23:08:39 +0530

    Map fragment removed

--- app/src/main/java/com/example/conferenceapp/FragmentLocationDetails.java
+++ app/src/main/java/com/example/conferenceapp/FragmentLocationDetails.java
@@ -13,23 +13,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.otaliastudios.zoom.ZoomImageView;
-
 public class FragmentLocationDetails extends Fragment{
 
-    private final LatLng LOCATION = new LatLng(45.50386, -73.56096889999998);
-    private GoogleMap map;
-    private Marker myMarker;
-    private ImageView mapView;
 
     @Nullable
     @Override
@@ -43,52 +28,6 @@ public class FragmentLocationDetails extends Fragment{
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-        SupportMapFragment mSupportMapFragment;
 
-        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapwhere);
-        if (mSupportMapFragment == null) {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            mSupportMapFragment = SupportMapFragment.newInstance();
-            fragmentTransaction.replace(R.id.mapwhere, mSupportMapFragment).commit();
-        }
-
-        if (mSupportMapFragment != null) {
-            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    if (googleMap != null) {
-
-                        googleMap.getUiSettings().setAllGesturesEnabled(true);
-
-                        CameraPosition cameraPosition = new CameraPosition.Builder().target(LOCATION).zoom(15.0f).build();
-                        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
-                        googleMap.addMarker(new MarkerOptions().position(LOCATION).title("Find us here!"));
-                        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                        googleMap.moveCamera(cameraUpdate);
-
-                        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                            @Override
-                            public boolean onMarkerClick(Marker marker) {
-                                if (marker.getTitle().equals("Find us here!")) {
-                                    // if marker source is clicked
-                                    Toast.makeText(getActivity(), marker.getTitle(), Toast.LENGTH_SHORT).show();
-                                    Uri gmmIntentUri = Uri.parse("google.navigation:q=" + "Palais des congrès de Montréal");
-                                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                                    mapIntent.setPackage("com.google.android.apps.maps");
-                                    startActivity(mapIntent);
-                                }
-                                return true;
-                            }
-                        });
-
-                    }
-
-                }
-            });
-
-            ZoomImageView mapView = (ZoomImageView) view.findViewById(R.id.mapImageView);
-            mapView.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.map));
-        }
     }
 }
