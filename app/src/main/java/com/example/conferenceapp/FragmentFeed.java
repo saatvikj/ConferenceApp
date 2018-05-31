e1c3eb93b022370cb19f7e6697908d34231c5952
Meghna Gupta <meghnagupta64@gmail.com>
2018-05-31 14:18:34 +0530

    Create Post page

--- app/src/main/java/com/example/conferenceapp/FragmentFeed.java
+++ app/src/main/java/com/example/conferenceapp/FragmentFeed.java
@@ -1,7 +1,9 @@
 package com.example.conferenceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +20,8 @@ public class FragmentFeed extends Fragment {
 
     URI uri = null;
     URL url = null;
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
 
     @Nullable
     @Override
@@ -30,6 +34,7 @@ public class FragmentFeed extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         RecyclerView recyclerView = view.findViewById(R.id.mFeedView);
+        fab = view.findViewById(R.id.fab);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         List<FeedPost> posts = new ArrayList<>();
         posts.add(new FeedPost("Saatvik Jain", "14h ago", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", null, 10, null));
@@ -44,5 +49,30 @@ public class FragmentFeed extends Fragment {
         posts.add(new FeedPost("Meghna Gupta", "4h ago", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.", null, 20, null));
         PostAdapter postAdapter = new PostAdapter(posts,view.getContext());
         recyclerView.setAdapter(postAdapter);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener(){
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy){
+                if (dy > 0 ||dy<0 && fab.isShown())
+                    fab.hide();
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+                if (newState == RecyclerView.SCROLL_STATE_IDLE){
+                    fab.show();
+                }
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), CreatePost.class);
+            }
+        });
+
     }
 }
