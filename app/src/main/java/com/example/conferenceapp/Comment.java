7ce6ac593754b5eab3f82603dfc92fc14b1d0c68
Meghna Gupta <meghnagupta64@gmail.com>
2018-05-31 12:28:05 +0530

    Comment class added

---
+++ app/src/main/java/com/example/conferenceapp/Comment.java
@@ -0,0 +1,27 @@
+package com.example.conferenceapp;
+
+public class Comment {
+
+    public String name;
+    public String timestamp;
+    public String text;
+
+    public Comment(String name, String timestamp, String text) {
+        this.name = name;
+        this.timestamp = timestamp;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
