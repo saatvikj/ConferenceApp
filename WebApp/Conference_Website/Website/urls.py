3aeb41f6902c473edb0f450072f54ea31b0cd431
Saatvik Jain <saatvik16261@iiitd.ac.in>
2018-08-02 14:47:45 +0530

    Added all pages of static website

--- WebApp/Conference_Website/Website/urls.py
+++ WebApp/Conference_Website/Website/urls.py
@@ -7,8 +7,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^overview/', TemplateView.as_view(template_name='overview.html'),name='overview'),
-    url(r'^index3/', views.Index3PageView.as_view()),
-    url(r'^index4/', views.Index4PageView.as_view()),
-    url(r'^index5/', views.Index5PageView.as_view()),
-    url(r'^thank_you', views.ThankYouPageView.as_view()),
+    url(r'^guide/', TemplateView.as_view(template_name='guide.html'),name='guide'),
+    url(r'^about/', TemplateView.as_view(template_name='about.html'),name='about'),
+    url(r'^faq/', TemplateView.as_view(template_name='faq.html'),name='faq'),
+    url(r'^contact/', TemplateView.as_view(template_name='contact.html'),name='contact'),
 ]
