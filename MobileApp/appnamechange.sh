2a11192b8d5b40422bdc843a6954e6c102ebb6b9
Meghna Gupta <meghnagupta64@gmail.com>
2018-11-19 17:50:13 +0530

    Feed content changed

--- MobileApp/appnamechange.sh
+++ MobileApp/appnamechange.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
 cd ../../MobileApp/app/src/main/res/values
-pwd
+echo $1
 sed -i 's/<string name="app_name">[a-zA-Z0-9  ]*['"'"']*[a-zA-Z0-9  ]*/<string name="app_name">'""$1""'/g' strings.xml
 cd ../../../../../../WebApp/Conference_Website/
