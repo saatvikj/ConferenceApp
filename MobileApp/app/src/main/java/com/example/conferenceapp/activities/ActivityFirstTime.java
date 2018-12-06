d4f886beb45877ded6294b91653ff6678b1aea5a
Saatvik Jain <saatvik16261@iiitd.ac.in>
2018-12-06 21:12:32 +0530

    Added progress bar for first time login

--- MobileApp/app/src/main/java/com/example/conferenceapp/activities/ActivityFirstTime.java
+++ MobileApp/app/src/main/java/com/example/conferenceapp/activities/ActivityFirstTime.java
@@ -1,5 +1,6 @@
 package com.example.conferenceapp.activities;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.conferenceapp.models.User;
@@ -23,6 +25,7 @@ public class ActivityFirstTime extends AppCompatActivity {
     EditText emailField;
     Button button;
     Conference conference;
+    ProgressDialog progressBar;
     private DatabaseReference mDatabase;
     private String final_email_id;
     private String final_joining_code;
@@ -35,7 +38,10 @@ public class ActivityFirstTime extends AppCompatActivity {
         context = getApplicationContext();
         emailField = findViewById(R.id.editTextUsername);
         button = findViewById(R.id.joiningCodeButton);
-
+        progressBar = new ProgressDialog(this);
+        progressBar.setIndeterminate(true);
+        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressBar.setMessage("Processing information");
         try {
             conference = ConferenceCSVParser.parseCSV(context);
 
@@ -45,6 +51,7 @@ public class ActivityFirstTime extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.show();
                 final String emailId = emailField.getText().toString();
                 final String conference_id = conference.getConference_id();
                 emailField.setText("");
@@ -67,6 +74,7 @@ public class ActivityFirstTime extends AppCompatActivity {
                                 button.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
+                                        progressBar.hide();
                                         String user_input = emailField.getText().toString();
                                         if(user_input.equals(final_joining_code)){
                                             Intent intent = new Intent(ActivityFirstTime.this, ActivitySetPassword.class);
